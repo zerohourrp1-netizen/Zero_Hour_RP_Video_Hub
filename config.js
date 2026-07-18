68b70cb27ee262e4c7d963cb59d629e032445bd3
zerohourrp1-netizen <zerohourrp1@gmail.com>
2026-07-18 17:05:12 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.ZERO_HOUR_CONFIG = {
-  supabaseUrl: "https://idapadtlcwuhenuzemuk.supabase.co",
-  supabaseAnonKey: "sb_publishable_sfNZVb54HnHO5F-7SoOpbQ_Zj_eAhrl",
-  discordUrl: "https://discord.gg/8rCsghjneQ",
-  fivemUrl: "https://servers.fivem.net/servers/detail/oaxomey",
-  siteName: "Zero Hour RP Videos",
-  defaultThumbnail: "zero-hour-logo.png"
-};

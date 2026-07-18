ce5f519634d81508371c7277c83d11eb2ec543ec
zerohourrp1-netizen <zerohourrp1@gmail.com>
2026-07-18 16:32:09 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ZERO_HOUR_CONFIG = {
-  supabaseUrl: "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE",
+  supabaseUrl: "https://idapadtlcwuhenuzemuk.supabase.co",
   supabaseAnonKey: "sb_publishable_sfNZVb54HnHO5F-7SoOpbQ_Zj_eAhrl",
   discordUrl: "https://discord.gg/8rCsghjneQ",
   fivemUrl: "https://servers.fivem.net/servers/detail/oaxomey",
